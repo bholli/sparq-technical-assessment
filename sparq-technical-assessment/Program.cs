fb9659e636d828e5f4e5f44e44f80e0d2c1cde36
Brandon C. Holliday <bholli@gmail.com>
2023-08-16 21:57:32 -0500

    Update Program.cs
    updated tests for excptions

--- sparq-technical-assessment/Program.cs
+++ sparq-technical-assessment/Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 using sparq_technical_assessment;
 
 //code assumes TVlalue is string
@@ -7,21 +7,41 @@ Console.WriteLine("welcome to the sparq technical assessment");
 
 StringMap<string> map = new StringMap<string>();
 
+//default value
 map.DefaultValue = "default";
+
+//write default value
 Console.WriteLine("Default value: " + map.DefaultValue);
 
-
+//add element
 map.AddElement("something", "good");
-Console.WriteLine(map.GetValue("brandon"));
 
-//count
+//write value of key == "something"
+Console.WriteLine(map.GetValue("something"));
+
+//write value of key == "something else"
+Console.WriteLine(map.GetValue("something else"));
+
+//count; should == 1
 Console.WriteLine("Count: " + map.Count);
 
 //add element
-map.AddElement("crystal", "holliday");
+map.AddElement("everything", "good");
 
-//count
+//count should == 2
 Console.WriteLine("Count: " + map.Count);
 
-//attempt to add element with empty key; should throw exception
+//remove element
+map.RemoveElement("something");
+
+//count; should == 1
+Console.WriteLine("Count: " + map.Count);
+
+//EXCEPTION: attempt to add element with null key; should throw exception
+map.AddElement(null, "boom");
+
+//EXCEPTION: attempt to add element with empty key; should throw exception
 map.AddElement("", "boom");
+
+//EXCEPTION: attempt to add element with duplicate key; should throw exception
+map.AddElement("something", "bad");
